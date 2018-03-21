dd06ae006635575dc36b096c423cd689525b5d37
Kelly Bennett <cornerkick87@gmail.com>
2018-03-20 19:57:44 -0700

    adding two more pictures

--- app.js
+++ app.js
@@ -1,45 +1,57 @@
 'use strict';
 
-// array to hold all of the Goat objects
+// array to hold all of the Merch objects
 Merchandise.allMerchandise = [];
 
-// make a Goat constructor for goat objects
+// make a Goat constructor for Merch objects
 function Merchandise(filepath, name) {
   this.filepath = filepath;
   this.name = name;
   Merchandise.allMerchandise.push(this);
 }
 
-// new instances of Goats
+// new instances of Merch
 new Merchandise('img/bag.jpg', 'r2d2 bag');
 new Merchandise('img/banana.jpg', 'banana cutter');
 new Merchandise('img/bathroom.jpg', 'bathroom ipad holder');
 new Merchandise('img/boots.jpg', 'Sandal boots');
 new Merchandise('img/bubblegum.jpg', 'meatball gym');
-new Merchandise('img/breakfast.jpeg', 'breakfast maker');
-// new Merchandise('img/wine-glass.jpeg', 'wine glass');
-// new Merchandise('img/water-can.jpeg', 'watering can');
-// new Merchandise('img/usb.gif', 'usb');
-// new Merchandise('img/unicorn.jpg', 'unicorn');
-// new Merchandise('img/tauntaun.jpg', 'tauntaun');
-// new Merchandise('img/sweep.png', 'sweet');
-// new Merchandise('img/shark.jpg', 'shark Blanket');
-// new Merchandise('img/scissors.jpg', 'scissors');
+new Merchandise('img/breakfast.jpg', 'breakfast maker');
+new Merchandise('img/wine-glass.jpg', 'wine glass');
+new Merchandise('img/water-can.jpg', 'watering can');
+new Merchandise('img/usb.gif', 'usb');
+new Merchandise('img/unicorn.jpg', 'unicorn');
+new Merchandise('img/tauntaun.jpg', 'tauntaun');
+new Merchandise('img/sweep.png', 'sweet');
+new Merchandise('img/shark.jpg', 'shark Blanket');
+new Merchandise('img/scissors.jpg', 'scissors');
 
 
 // access the element from the DOM
-var imgElement = document.getElementById('Merchandise-pix');
+var leftElement = document.getElementById('left');
+var middleElement= document.getElementById('middle');
+var rightElement = document.getElementById('right');
 
 // add an event listener
-imgElement.addEventListener('click', randomMerchandise);
+leftElement.addEventListener('click', randomMerchandise);
+middleElement.addEventListener('click', randomMerchandise);
+rightElement.addEventListener('click', randomMerchandise);
 
 // callback function when image is clicked:
 function randomMerchandise() {
   // random number generator
   var randomIndex = Math.floor(Math.random() * Merchandise.allMerchandise.length);
+  var secondRandomIndex = Math.floor(Math.random() * Merchandise.allMerchandise.length);
+  var thirdRandomIndex = Math.floor(Math.random() * Merchandise.allMerchandise.length);
   // use the randomIndex to set the src and alt attributes of the imgElement
-  imgElement.src = Merchandise.allMerchandise[randomIndex].filepath;
-  imgElement.alt = Merchandise.allMerchandise[randomIndex].name;
+  leftElement.src = Merchandise.allMerchandise[randomIndex].filepath;
+  leftElement.alt = Merchandise.allMerchandise[randomIndex].name;
+  
+  middleElement.src = Merchandise.allMerchandise[secondRandomIndex].filepath;
+  middleElement.alt = Merchandise.allMerchandise[secondRandomIndex].name;
+  
+  rightElement.src = Merchandise.allMerchandise[thirdRandomIndex].filepath;
+  rightElement.alt = Merchandise.allMerchandise[thirdRandomIndex].name;
 }
 
 // render an image on page load
