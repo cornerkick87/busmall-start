bd88bc71aba0e649fd267be5c89589d839d81bc3
Kelly Bennett <cornerkick87@gmail.com>
2018-03-20 21:56:38 -0700

    working counter in js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ function Merchandise(filepath, name) {
   this.filepath = filepath;
   this.name = name;
   Merchandise.allMerchandise.push(this);
+  this.clickCounter = 0;
+  this.showCounter = 0;
 }
 
 // new instances of Merch
@@ -38,20 +40,40 @@ middleElement.addEventListener('click', randomMerchandise);
 rightElement.addEventListener('click', randomMerchandise);
 
 // callback function when image is clicked:
-function randomMerchandise() {
+function randomMerchandise(event) {
+  if(event !== undefined) {
+    var model = Merchandise.allMerchandise[event.target.dataset['merchandise']];
+    model.clickCounter ++;
+    console.log(Merchandise.allMerchandise);
+  }
+  
   // random number generator
   var randomIndex = Math.floor(Math.random() * Merchandise.allMerchandise.length);
   var secondRandomIndex = Math.floor(Math.random() * Merchandise.allMerchandise.length);
+  
+  while( secondRandomIndex === randomIndex) {
+    secondRandomIndex = Math.floor(Math.random() * Merchandise.allMerchandise.length);
+  }
   var thirdRandomIndex = Math.floor(Math.random() * Merchandise.allMerchandise.length);
+  
+  while( thirdRandomIndex === randomIndex || thirdRandomIndex === secondRandomIndex) {
+    thirdRandomIndex = Math.floor(Math.random() * Merchandise.allMerchandise.length);
+  }
   // use the randomIndex to set the src and alt attributes of the imgElement
   leftElement.src = Merchandise.allMerchandise[randomIndex].filepath;
   leftElement.alt = Merchandise.allMerchandise[randomIndex].name;
+  leftElement.dataset['merchandise'] = randomIndex;
+  Merchandise.allMerchandise[randomIndex].showCounter++;
   
   middleElement.src = Merchandise.allMerchandise[secondRandomIndex].filepath;
   middleElement.alt = Merchandise.allMerchandise[secondRandomIndex].name;
-  
+  middleElement.dataset['merchandise'] = secondRandomIndex;
+  Merchandise.allMerchandise[secondRandomIndex].showCounter++;
+
   rightElement.src = Merchandise.allMerchandise[thirdRandomIndex].filepath;
   rightElement.alt = Merchandise.allMerchandise[thirdRandomIndex].name;
+  rightElement.dataset['merchandise'] = thirdRandomIndex;
+  Merchandise.allMerchandise[thirdRandomIndex].showCounter++;
 }
 
 // render an image on page load
